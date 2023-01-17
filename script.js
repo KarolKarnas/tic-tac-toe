63ddcc31d7140d6db342df6ffb4e818f45909fd4
KarolKarnas <karol.karnas@gmail.com>
2023-01-17 12:33:39 +0100

    Create basic files and mockup of the game
    Create game board using html, style with css, position with grid.
    In script js write probably needed variables.

---
+++ script.js
@@ -0,0 +1,12 @@
+let a1 = 'i dont know';
+
+let gameBoardObject = { //module
+	gameBoard: [a1],
+};
+
+console.log(gameBoardObject.gameBoard[0]);
+
+let playerOne = {}; //factory
+let playerTwo = {}; //factory
+
+let controlGameFlow = {}; //module - displayController?
