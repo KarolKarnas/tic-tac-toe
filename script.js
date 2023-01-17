24b9b88f30d75236d778fe992e7192528062f2f1
KarolKarnas <karol.karnas@gmail.com>
2023-01-17 13:22:07 +0100

    create game-board with js
    create game-board with javascript function from fields stored in the array

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-let a1 = 'i dont know';
+let boardGameContainer = document.querySelector('.game-board');
 
-let gameBoardObject = { //module
-	gameBoard: [a1],
+console.log(boardGameContainer);
+
+let createGameBoard = function () {
+    let gameBoardFields = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9'];
+    for (let i = 0; i < 9; i++) {
+        //create one field
+        // console.log(gameBoardFields[i])
+        let boardField = document.createElement('div');
+        boardField.classList.add('field');
+        boardField.id = `${gameBoardFields[i]}`
+
+        // console.log(boardField);
+        boardGameContainer.appendChild(boardField);
+    }
 };
 
-console.log(gameBoardObject.gameBoard[0]);
+createGameBoard();
 
-let playerOne = {}; //factory
-let playerTwo = {}; //factory
+// // let a1 = 'i dont know';
 
-let controlGameFlow = {}; //module - displayController?
+// let gameBoardObject = {
+// 	//module
+// 	gameBoard: ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'],
+// };
+
+// console.log(gameBoardObject.gameBoard[0]);
+
+// let playerOne = {}; //factory
+// let playerTwo = {}; //factory
+
+// let controlGameFlow = {}; //module - displayController?
+
+
+// let createGameBoard = function () {
+// 	for (let i = 0; i < 9; i++) {
+// 		//create one field
+// 		let boardField = document.createElement('div');
+// 		boardField.classList.add('field');
+
+// 		console.log(boardField);
+// 		boardGameContainer.appendChild(boardField);
+// 	}
+// };
+
+
+
+
+
+
