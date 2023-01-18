fe2ea1ee6f5cf15a0ae7607a765a57c194564efe
KarolKarnas <karol.karnas@gmail.com>
2023-01-18 13:42:58 +0100

    Create game buttons, modify gameBoardFields array
    Create buttons: Clear Fields, Clear Array, New Game
    Add event listeners with according functions to the buttons
    
    Change array content from objects to empty strings - make easier manipulating content, objects were unnecessary there

--- script.js
+++ script.js
@@ -4,17 +4,7 @@ let gameBoard = (() => {
 	boardGameContainer.classList.add('game-board');
 	container.appendChild(boardGameContainer);
 
-	let gameBoardFields = [
-		{ mark: '' },
-		{ mark: '' },
-		{ mark: '' },
-		{ mark: '' },
-		{ mark: '' },
-		{ mark: '' },
-		{ mark: '' },
-		{ mark: '' },
-		{ mark: '' },
-	];
+	let gameBoardFields = ['', '', '', '', '', '', '', '', ''];
 
 	for (let i = 0; i < 9; i++) {
 		//create field
@@ -22,7 +12,7 @@ let gameBoard = (() => {
 		//add class id and mark
 		boardField.classList.add('field');
 		// boardField.id = `${gameBoardFields[i].id}`;
-		boardField.textContent = `${gameBoardFields[i].mark}`;
+		boardField.textContent = `${gameBoardFields[i]}`;
 		boardGameContainer.appendChild(boardField);
 	}
 
@@ -54,13 +44,34 @@ let newGame = () => {
 	console.log(p1);
 	let p2 = player('p2', 'x');
 	console.log(p2);
-
 	//DEFAULT current player
 
 	let currentPlayer = p1;
 
 	let allFields = document.querySelectorAll('.field');
 	// console.log(allFields);
+
+	// CLEAR FIELDS, CLEAR ARRAY, NEW GAME BUTTON
+
+	let clearFields = function () {
+		allFields.forEach((element) => (element.textContent = ''));
+	};
+
+	let clearFieldsBtn = document.querySelector('.clear-fields-btn');
+	clearFieldsBtn.addEventListener('click', clearFields);
+
+	let clearArray = function () {
+		gameBoard.gameBoardFields = gameBoard.gameBoardFields.map(() => '');
+		console.log(gameBoard.gameBoardFields);
+	};
+
+	let clearArrayBtn = document.querySelector('.clear-array-btn');
+	clearArrayBtn.addEventListener('click', clearArray);
+
+	let newGameBtn = document.querySelector('.new-game-btn');
+	newGameBtn.addEventListener('click', () => {
+		clearFields(), clearArray();
+	});
 
 	// add mark on the field, update array gameBoardFields
 	allFields.forEach((element, index) =>
@@ -88,102 +99,3 @@ let newGame = () => {
 };
 
 newGame();
-
-//create player ONE - factory
-// let playerOne = (name, mark) => {
-// 	const playerOneName = name;
-// 	const playerOneMark = mark;
-// 	const playerOneChange = () => gameBoard.changeMark(1, mark);
-
-// 	let allFields = document.querySelectorAll('.field');
-// 	allFields.forEach((field) =>
-// 		field.addEventListener('click', function () {
-// 			//check isEmpty
-// 			this.textContent = mark;
-// 			// gameFlow - check forWin,nextRound
-// 			//
-// 		})
-// 	    );
-
-// 	return { playerOneName, playerOneMark };
-// };
-
-// // Player Two
-
-// let playerTwo = (name, mark) => {
-// 	const playerTwoName = name;
-// 	const playerTwoMark = mark;
-// 	    let allFields = document.querySelectorAll('.field');
-// 	    allFields.forEach((field) =>
-// 	    field.addEventListener('click', function () {
-// 	        this.textContent = mark;
-// 	    })
-// 	);
-
-// 	return { playerTwoName, playerTwoMark };
-// };
-
-// let playerTwo = {}; //factory
-
-// let controlGameFlow = {}; //module - displayController?
-
-// createGameBoard();
-
-// let boardGameContainer = document.querySelector('.game-board');
-
-// console.log(boardGameContainer);
-
-//
-// let createGameBoard = function () {
-// 	for (let i = 0; i < 9; i++) {
-// 		//create one field
-// 		let gameBoardFields = [
-// 			{ name: 'f1', symbol: 'o' },
-// 			{ name: 'f2', symbol: 'x' },
-// 			{ name: 'f3', symbol: 'o' },
-// 			{ name: 'f4', symbol: 'x' },
-// 			{ name: 'f5', symbol: 'o' },
-// 			{ name: 'f6', symbol: 'x' },
-// 			{ name: 'f7', symbol: 'o' },
-// 			{ name: 'f8', symbol: 'x' },
-// 			{ name: 'f9', symbol: 'o' },
-// 		];
-// 		// console.log(gameBoardFields[i])
-// 		let boardField = document.createElement('div');
-// 		boardField.classList.add('field');
-// 		boardField.id = `${gameBoardFields[i].name}`;
-//         boardField.textContent = `${gameBoardFields[i].symbol}`
-
-// 		// console.log(boardField);
-// 		boardGameContainer.appendChild(boardField);
-// 	}
-// };
-
-// createGameBoard();
-
-// let gameBoardFields = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9'];
-
-// // let a1 = 'i dont know';
-
-// let gameBoardObject = {
-// 	//module
-// 	gameBoard: ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'],
-// };
-
-// console.log(gameBoardObject.gameBoard[0]);
-
-// let playerOne = {}; //factory
-// let playerTwo = {}; //factory
-
-// let controlGameFlow = {}; //module - displayController?
-
-// let createGameBoard = function () {
-// 	for (let i = 0; i < 9; i++) {
-// 		//create one field
-// 		let boardField = document.createElement('div');
-// 		boardField.classList.add('field');
-
-// 		console.log(boardField);
-// 		boardGameContainer.appendChild(boardField);
-// 	}
-// };
